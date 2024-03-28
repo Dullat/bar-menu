32d0713d452528158dcb933544fad30272238c8d
dullat <dullat@users.noreply.github.com>
2024-03-28 22:39:44 +0530

    update

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+
+  mode: 'development',
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 5911,
+    open: true, // open in def browser
+    hot: true, // auto load
+    compress: true, // it says compress
+    historyApiFallback: true,
+  },
+};
