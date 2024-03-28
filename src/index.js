86faa0eee146fcbb7319bb5daf93b5ee9aa26a63
dullat <dullat@users.noreply.github.com>
2024-03-29 01:26:39 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './scss/reset.scss';
+import './scss/magic.scss';
+
+let list = document.querySelector('.primary-nav').querySelectorAll('a');
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove('active');
+    this.classList.add('active');
+  });
+}
+
+list.forEach((item) => item.addEventListener('click', activeLink));
