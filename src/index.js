c4912c131f560cc05fb8b73d059a1d353372806b
dullat <dullat@users.noreply.github.com>
2024-03-29 02:56:06 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import './scss/reset.scss';
 import './scss/magic.scss';
 
 let list = document.querySelector('.primary-nav').querySelectorAll('a');
+let cssRule = ``;
 
-function activeLink() {
+function activeLink(receved) {
   list.forEach((item) => {
     item.classList.remove('active');
-    this.classList.add('active');
+    receved.classList.add('active');
   });
 }
 
-list.forEach((item) => item.addEventListener('click', activeLink));
+list.forEach((item, index) =>
+  item.addEventListener('click', () => {
+    activeLink(item);
+    console.log(item.clientWidth);
+    console.log(item.parentNode.clientWidth);
+    let toMove = (item.parentNode.clientWidth - 64 - item.clientWidth * 5) / 4;
+    console.log(toMove);
+    if (index === 0) {
+      toMove = 0;
+    }
+    document.querySelector('indicator').style.transform =
+      `translateX(${(toMove + item.clientWidth) * index}px)`;
+
+    console.log(document.querySelector('indicator'));
+  }),
+);
